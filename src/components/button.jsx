3c73ba784d5780191c6e06989096f1ef03cc261c
Mohammed19J <mohammed.jaber1902@gmail.com>
2025-01-05 16:07:34 +0200

    V0.6

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,11 +2,12 @@ const Button = ({ text, onClick, color, className = "" }) => {
     return (
         <button
             onClick={onClick}
-            className={`text-white bg-${color}-500 hover:bg-${color}-600 px-4 py-2 rounded transition-colors duration-200 ${className}`}
+            className={`px-4 py-2 rounded-lg font-medium transition-colors duration-200
+                text-gray-700 hover:bg-${color}-200
+                dark:text-gray-300 dark:hover:bg-${color}-700
+                ${className}`}
         >
             {text}
         </button>
     );
 };
-
-export default Button;
